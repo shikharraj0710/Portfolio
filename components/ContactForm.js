5e3139efb709ca4ed251bd182b399e7dd5412d6d
shiharraj0710 <shikharraj921@gmail.com>
2022-12-27 21:37:36 +0530

    Contact Form changed

--- components/ContactForm.js
+++ components/ContactForm.js
@@ -10,10 +10,11 @@ export default function ContactForm({ hireRef }) {
     register,
     handleSubmit,
     formState: { errors },
-    reset
+    reset,
   } = useForm();
   const [info, setInfo] = useState({});
   const [nameErr, setNameErr] = useState(false);
+  const [emailErr, setEmailErr] = useState(false);
   const [messageSent, setMessageSent] = useState(false);
   const form = useRef();
   const { ref, inView } = useInView({
@@ -34,29 +35,34 @@ export default function ContactForm({ hireRef }) {
   }, [inView]);
 
   const handleForm = (e) => {
-    let pattern = /^[a-z]+$/i;
-    if (
-      e.target.name == "name" &&
-      !pattern.test(e.target.value) &&
-      e.target.value?.trim() != ""
-    )
+    let namePattern = /^[A-Za-z\s]*$/;
+    if (e.target.name === "email") setEmailErr(false);
+    if (e.target.name === "name" && !namePattern.test(e.target.value)) {
       setNameErr(true);
+    }
     else {
-      setNameErr(false);
+
       setInfo((prev) => ({ ...prev, [e.target.name]: e.target.value }));
     }
+    if (namePattern.test(e.target.value)) setNameErr(false)
   };
 
   const handleFormSubmit = async (e) => {
-    setMessageSent(true);
-    let pattern = /^[a-z]+$/i;
+    let pattern = /^[A-Za-z\s]*$/;
+    let emailPattern = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
+    if (!emailPattern.test(info?.email)) {
+      setEmailErr(true);
+      return;
+    }
     if (
       Object.keys(info).includes("name") &&
       Object.keys(info).includes("email") &&
-      pattern.test(info?.name)
+      pattern.test(info?.name) &&
+      !nameErr
     ) {
+      setEmailErr(false);
       setNameErr(false);
-      info?.message?.toString().trim() == "" &&
+      info?.message?.toString().trim() === "" &&
         setInfo((prev) => ({ ...prev, message: "" }));
       const response = await fetch(`${server}/api/form`, {
         method: "POST",
@@ -64,7 +70,6 @@ export default function ContactForm({ hireRef }) {
         body: JSON.stringify(info),
       });
       const data = await response.json();
-      setNameErr(false);
       document.getElementById("my-modal").classList.remove("hidden");
       document
         .getElementById("modal-content")
@@ -75,8 +80,9 @@ export default function ContactForm({ hireRef }) {
       document
         .getElementById("modal-content")
         .classList.add("modal-animation-open");
-      setInfo(() => {});
+      setInfo(() => { });
       if (data?.statusCode === 200) {
+        setMessageSent(true);
         emailjs
           .sendForm(
             process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID,
@@ -92,7 +98,7 @@ export default function ContactForm({ hireRef }) {
               console.log(error.text);
             }
           )
-          .finally(() => {setMessageSent(false); document.getElementById("form").reset(); reset()});
+          .finally(() => { setMessageSent(false); document.getElementById("form").reset(); reset() });
       }
     } else {
       setNameErr(true);
@@ -101,125 +107,122 @@ export default function ContactForm({ hireRef }) {
 
   return (
     <>
-    <div ref={ref}>
-      <motion.div animate={animation} ref={hireRef} className="border-4  rounded-4 border-darkGolden  rounded w-[95%] lg:w-[80%] mx-auto py-8 px-6 flex flex-col md:flex-row items-start md:items-center md:mb-20 mb-10">
-        <div className="font-heading text-center  text-4xl capitalize text-customBlack tracking-wide md:w-[50%] mb-10 md:mb-0 ">
-          want to get in touch ?
-        </div>
-        <div className="font-content w-full md:w-auto md:grow">
-          <form id="form" autoComplete="off" ref={form}>
-            <div className="flex flex-col md:flex-row gap-4 md:px-7 justify-around md:mb-5">
-              <div className="flex flex-col gap-2 w-full">
-                <label htmlFor="name">Full Name</label>
-                <input
-                  autoComplete="new-password"
-                  className="form-input rounded-md text-sm outline-none"
-                  onInput={handleForm}
-                  type="text"
-                  id="name"
-                  placeholder="Enter your Full name"
-                  title="Enter your full name"
-                  name="name"
-                  {...register("name", {
-                    required: {
-                      value: true,
-                      message: "Name cannot be empty",
-                    },
-                  })}
-                />
-                {errors?.name && (
-                  <div
-                    id="nameError"
-                    className="text-customRed font-heading tracking-wider text-xs"
-                  >
-                    {errors?.name?.message}
-                  </div>
-                )}
-                {nameErr && (
-                  <div
-                    id="nameError"
-                    className="text-customRed font-heading tracking-wider text-xs"
-                  >
-                    Name is incorrect
-                  </div>
-                )}
-              </div>
-              <div className="flex flex-col gap-2 w-full">
-                <label htmlFor="email">Email</label>
-                <input
-                  autoComplete="new-password"
-                  className="form-input rounded-md text-sm outline-none"
-                  onInput={handleForm}
-                  type="email"
-                  placeholder="Enter your email"
-                  title="Enter your primary email"
-                  name="email"
-                  {...register("email", {
-                    required: {
-                      value: true,
-                      message: "Email cannot be empty",
-                    },
-                  })}
-                />
-                {errors?.email && (
-                  <div
-                    id="nameError"
-                    className="text-customRed font-heading tracking-wider text-xs"
-                  >
-                    {errors?.email?.message}
-                  </div>
-                )}
-              </div>
-            </div>
-            <div className="flex flex-col gap-2 md:px-7 mt-6 md:mt-4 lg:mt-0">
-              <label htmlFor="message">Message</label>
-              <textarea
-                spellCheck="off"
-                onInput={handleForm}
-                placeholder="Enter your message"
-                className="rounded-md resize-none outline-none"
-                name="message"
-                {...register("message", {
-                  required: {
-                    value: true,
-                    message: "Message cannot be empty",
-                  },
-                })}
-              />
-              {errors?.message && (
-                <div
-                  id="nameError"
-                  className="text-customRed font-heading tracking-wider text-xs"
-                >
-                  {errors?.message?.message}
+      <div ref={ref}>
+        <motion.div
+          animate={animation}
+          ref={hireRef}
+          className="border-4  rounded-4 border-darkGolden  rounded w-[95%] lg:w-[80%] mx-auto py-8 px-6 flex flex-col md:flex-row items-start md:items-center md:mb-20 mb-10"
+        >
+          <div className="font-heading text-center  text-4xl capitalize text-customBlack tracking-wide md:w-[50%] mb-10 md:mb-0 ">
+            want to get in touch ?
+          </div>
+          <div className="font-content w-full md:w-auto md:grow">
+            <form id="form" autoComplete="off" ref={form}>
+              <div className="flex flex-col md:flex-row gap-4 md:px-7 justify-around md:mb-5">
+                <div className="flex flex-col gap-2 w-full">
+                  <label htmlFor="name">Full Name</label>
+                  <input
+                    autoComplete="new-password"
+                    className="form-input rounded-md text-sm outline-none"
+                    onInput={handleForm}
+                    type="text"
+                    id="name"
+                    placeholder="Enter your Full name"
+                    title="Enter your full name"
+                    name="name"
+                    {...register("name", {
+                      required: {
+                        value: true,
+                        message: "Name cannot be empty",
+                      },
+                    })}
+                  />
+                  {errors?.name && (
+                    <div className="text-customRed font-heading tracking-wider text-xs">
+                      {errors?.name?.message}
+                    </div>
+                  )}
+                  {nameErr && (
+                    <div className="text-customRed font-heading tracking-wider text-xs">
+                      Name is incorrect
+                    </div>
+                  )}
                 </div>
-              )}
-            </div>
+                <div className="flex flex-col gap-2 w-full">
+                  <label htmlFor="email">Email</label>
+                  <input
+                    autoComplete="new-password"
+                    className="form-input rounded-md text-sm outline-none"
+                    onInput={handleForm}
+                    type="email"
+                    placeholder="Enter your email"
+                    title="Enter your primary email"
+                    name="email"
+                    {...register("email", {
+                      required: {
+                        value: true,
+                        message: "Email cannot be empty",
+                      },
+                    })}
+                  />
+                  {errors?.email && (
+                    <div className="text-customRed font-heading tracking-wider text-xs">
+                      {errors?.email?.message}
+                    </div>
+                  )}
+                  {emailErr && (
+                    <div className="text-customRed font-heading tracking-wider text-xs">
+                      Email is incorrect
+                    </div>
+                  )}
+                </div>
+              </div>
+              <div className="flex flex-col gap-2 md:px-7 mt-6 md:mt-4 lg:mt-0">
+                <label htmlFor="message">Message</label>
+                <textarea
+                  spellCheck="off"
+                  onInput={handleForm}
+                  placeholder="Enter your message"
+                  className="rounded-md resize-none outline-none"
+                  name="message"
+                  {...register("message", {
+                    required: {
+                      value: true,
+                      message: "Message cannot be empty",
+                    },
+                  })}
+                />
+                {errors?.message && (
+                  <div className="text-customRed font-heading tracking-wider text-xs">
+                    {errors?.message?.message}
+                  </div>
+                )}
+              </div>
 
-            <div className="flex justify-center lg:justify-end md:px-7 md:mt-5 mt-6 lg:mt-4">
-              {messageSent ? (
-                <button
-                  type="button"
-                  disabled
-                  className="px-4 uppercase font-sans border-2 border-darkGolden rounded-md p-2 hover:bg-orange-400 hover:text-white transition ease-in-out delay-50  hover:-translate-y-1 hover:scale-110 tracking-widest"
-                >
-                  sending...
-                </button>
-              ) : (
-                <button
-                  type="button"
-                  onClick={handleSubmit(handleFormSubmit)}
-                  id="open-btn"
-                  className="px-4 uppercase font-sans border-2 border-darkGolden rounded-md p-2 hover:bg-orange-400 hover:text-white transition ease-in-out delay-50  hover:-translate-y-1 hover:scale-110 tracking-widest"
-                >
-                  send message
-                </button>
-              )}
-            </div>
-          </form>
-        </div>
-      </motion.div>
-    </div>
+              <div className="flex justify-center lg:justify-end md:px-7 md:mt-5 mt-6 lg:mt-4">
+                {messageSent ? (
+                  <button
+                    type="button"
+                    disabled
+                    className="px-4 animate-bounce uppercase font-sans border-2 border-darkGolden rounded-md p-2 cursor-not-allowed transition ease-in-out delay-50  tracking-widest"
+                  >
+                    sending...
+                  </button>
+                ) : (
+                  <button
+                    type="button"
+                    onClick={handleSubmit(handleFormSubmit)}
+                    id="open-btn"
+                    className="px-4 uppercase font-sans border-2 border-darkGolden rounded-md p-2 hover:bg-orange-400 hover:text-white transition ease-in-out delay-50  hover:-translate-y-1 hover:scale-110 tracking-widest"
+                  >
+                    send message
+                  </button>
+                )}
+              </div>
+            </form>
+          </div>
+        </motion.div>
+      </div>
     </>
   );
 }
--- pages/contact.js
+++ pages/contact.js
@@ -18,6 +18,7 @@ export default function Contact() {
   const [nameErr, setNameErr] = useState(false);
   const [show, setShow] = useState(false);
   const form = useRef();
+  const [emailErr, setEmailErr] = useState(false);
   const [messageSent, setMessageSent] = useState(false);
   const { ref, inView } = useInView({
     threshold: 0.5,
@@ -29,7 +30,7 @@ export default function Contact() {
       animation.start({
         opacity: 1,
         x: 0,
-        transition: { type: "spring", duration: 3, bounce: 1, delay: 1},
+        transition: { type: "spring", duration: 3, bounce: 1, delay: 1 },
       });
     } else {
       animation.start({ x: "-100vw", opacity: 0.2 });
@@ -37,24 +38,35 @@ export default function Contact() {
   }, [inView]);
 
   const handleForm = (e) => {
-    let pattern = /^[a-z]+$/i;
-    if(e.target.name == "name" && !pattern.test(e.target.value) && e.target.value?.trim() != '')  setNameErr(true) 
-    else {
-        setNameErr(false)
-        setInfo((prev) => ({ ...prev, [e.target.name]: e.target.value }))
+    let namePattern = /^[A-Za-z\s]*$/;
+    if (e.target.name === "email") setEmailErr(false);
+    if (e.target.name === "name" && !namePattern.test(e.target.value)) {
+      setNameErr(true);
     }
+    else {
+
+      setInfo((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+    }
+    if (namePattern.test(e.target.value)) setNameErr(false)
   };
 
+
   const handleFormSubmit = async (e) => {
-    setMessageSent(true);
-    let pattern = /^[a-z]+$/i;
+    let pattern = /^[A-Za-z\s]*$/;
+    let emailPattern = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
+    if (!emailPattern.test(info?.email)) {
+      setEmailErr(true);
+      return;
+    }
     if (
       Object.keys(info).includes("name") &&
       Object.keys(info).includes("email") &&
-      pattern.test(info?.name)
+      pattern.test(info?.name) &&
+      !nameErr
     ) {
+      setEmailErr(false);
       setNameErr(false);
-      info?.message?.toString().trim() == "" &&
+      info?.message?.toString().trim() === "" &&
         setInfo((prev) => ({ ...prev, message: "" }));
       const response = await fetch(`${server}/api/form`, {
         method: "POST",
@@ -62,7 +74,6 @@ export default function Contact() {
         body: JSON.stringify(info),
       });
       const data = await response.json();
-      setNameErr(false);
       document.getElementById("my-modal").classList.remove("hidden");
       document
         .getElementById("modal-content")
@@ -73,8 +84,9 @@ export default function Contact() {
       document
         .getElementById("modal-content")
         .classList.add("modal-animation-open");
-      setInfo(() => {});
+      setInfo(() => { });
       if (data?.statusCode === 200) {
+        setMessageSent(true);
         emailjs
           .sendForm(
             process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID,
@@ -90,7 +102,7 @@ export default function Contact() {
               console.log(error.text);
             }
           )
-          .finally(() => {setMessageSent(false); document.getElementById("form").reset(); reset()});
+          .finally(() => { setMessageSent(false); document.getElementById("form").reset(); reset() });
       }
     } else {
       setNameErr(true);
@@ -99,127 +111,128 @@ export default function Contact() {
 
   return (
     <>
-       <Head><title>Contact</title></Head>
-       <Modal  show={show} setShow={setShow}/>
-       <div ref={ref}>
-      <motion.div animate={animation} className="border-4 mt-10 lg:mt-20 rounded-4 border-darkGolden  rounded-lg w-[95%] lg:w-[80%] mx-auto py-8 px-6 flex flex-col md:flex-row items-start md:items-center md:mb-20 mb-10">
-        <div className="font-heading text-center  text-4xl capitalize text-customBlack tracking-wide md:w-[50%] mb-10 md:mb-0 ">
-          want to get in touch ?
-        </div>
-        <div className="font-content w-full md:w-auto md:grow">
-          <form id="form" autoComplete="off" ref={form}>
-            <div className="flex  flex-col md:flex-row gap-4 md:px-7 justify-around md:mb-5">
-              <div className="flex flex-col gap-2 w-full">
-                <label htmlFor="name">Full Name</label>
-                <input
-                  autoComplete="new-password"
-                  className="form-input rounded-md text-sm outline-none"
+      <Head><title>Contact</title></Head>
+      <Modal show={show} setShow={setShow} />
+      <div ref={ref}>
+        <motion.div animate={animation} className="border-4 mt-10 lg:mt-20 rounded-4 border-darkGolden  rounded-lg w-[95%] lg:w-[80%] mx-auto py-8 px-6 flex flex-col md:flex-row items-start md:items-center md:mb-20 mb-10">
+          <div className="font-heading text-center  text-4xl capitalize text-customBlack tracking-wide md:w-[50%] mb-10 md:mb-0 ">
+            want to get in touch ?
+          </div>
+          <div className="font-content w-full md:w-auto md:grow">
+            <form id="form" autoComplete="off" ref={form}>
+              <div className="flex  flex-col md:flex-row gap-4 md:px-7 justify-around md:mb-5">
+                <div className="flex flex-col gap-2 w-full">
+                  <label htmlFor="name">Full Name</label>
+                  <input
+                    autoComplete="new-password"
+                    className="form-input rounded-md text-sm outline-none"
+                    onInput={handleForm}
+                    type="text"
+                    id="name"
+                    placeholder="Enter your Full name"
+                    title="Enter your full name"
+                    name="name"
+                    {...register("name", {
+                      required: {
+                        value: true,
+                        message: "Name cannot be empty"
+                      }
+                    })}
+                  />
+                  {errors?.name && (
+                    <div
+                      className="text-customRed font-heading tracking-wider text-xs"
+                    >
+                      {errors?.name?.message}
+                    </div>
+                  )}
+                  {nameErr && (
+                    <div
+                      className="text-customRed font-heading tracking-wider text-xs"
+                    >
+                      Name is incorrect
+                    </div>
+                  )}
+                </div>
+                <div className="flex flex-col gap-2 w-full">
+                  <label htmlFor="email">Email</label>
+                  <input
+                    autoComplete="new-password"
+                    className="form-input rounded-md text-sm outline-none"
+                    onInput={handleForm}
+                    type="email"
+                    placeholder="Enter your email"
+                    title="Enter your primary email"
+
+                    name="email"
+                    {...register("email", {
+                      required: {
+                        value: true,
+                        message: "Email cannot be empty"
+                      }
+                    })}
+                  />
+                  {errors?.email && (
+                    <div
+                      className="text-customRed font-heading tracking-wider text-xs"
+                    >
+                      {errors?.email?.message}
+                    </div>
+                  )}
+                  {emailErr && (
+                    <div className="text-customRed font-heading tracking-wider text-xs">
+                      Email is incorrect
+                    </div>
+                  )}
+                </div>
+              </div>
+              <div className="flex flex-col gap-2 md:px-7 mt-6 lg:mt-0">
+                <label htmlFor="message">Message</label>
+                <textarea
+                  spellCheck="off"
                   onInput={handleForm}
-                  type="text"
-                  id="name"
-                  placeholder="Enter your Full name"
-                  title="Enter your full name"
-                  name="name"
-                  {...register("name", {
-                    required : {
-                      value : true,
-                      message : "Name cannot be empty"
+                  placeholder="Enter your message"
+                  className="rounded-md resize-none outline-none"
+                  name="message"
+                  {...register("message", {
+                    required: {
+                      value: true,
+                      message: "Message cannot be empty"
                     }
                   })}
                 />
-                 {errors?.name && (
-                  <div
-                    id="nameError"
-                    className="text-customRed font-heading tracking-wider text-xs"
-                  >
-                    {errors?.name?.message}
-                  </div>
-                )}
-                {nameErr && (
-                  <div
-                    id="nameError"
-                    className="text-customRed font-heading tracking-wider text-xs"
-                  >
-                    Name is incorrect
-                  </div>
-                )}
-              </div>
-              <div className="flex flex-col gap-2 w-full">
-                <label htmlFor="email">Email</label>
-                <input
-                  autoComplete="new-password"
-                  className="form-input rounded-md text-sm outline-none"
-                  onInput={handleForm}
-                  type="email"
-                  placeholder="Enter your email"
-                  title="Enter your primary email"
-                  
-                  name="email"
-                  {...register("email", {
-                    required : {
-                      value : true,
-                      message : "Email cannot be empty"
-                    }
-                  })}
-                />
-                {errors?.email && (
-                  <div
-                    id="nameError"
-                    className="text-customRed font-heading tracking-wider text-xs"
-                  >
-                    {errors?.email?.message}
-                  </div>
-                )}
-              </div>
-            </div>
-            <div className="flex flex-col gap-2 md:px-7 mt-6 lg:mt-0">
-              <label htmlFor="message">Message</label>
-              <textarea
-                spellCheck="off"
-                onInput={handleForm}
-                placeholder="Enter your message"
-                className="rounded-md resize-none outline-none"
-                name="message"
-                {...register("message", {
-                  required : {
-                    value : true,
-                    message : "Message cannot be empty"
-                  }
-                })}
-              />
                 {errors?.message && (
                   <div
-                    id="nameError"
                     className="text-customRed font-heading tracking-wider text-xs"
                   >
                     {errors?.message?.message}
                   </div>
                 )}
-            </div>
+              </div>
 
-            <div className="flex justify-center md:justify-end md:px-7 md:mt-5 mt-6 ">
-            {messageSent ? (
-                <button
-                  type="button"
-                  disabled
-                  className="px-4 uppercase font-sans border-2 border-darkGolden rounded-md p-2 hover:bg-orange-400 hover:text-white transition ease-in-out delay-50  hover:-translate-y-1 hover:scale-110 tracking-widest"
-                >
-                  sending...
-                </button>
-              ) : (
-                <button
-                  type="button"
-                  onClick={handleSubmit(handleFormSubmit)}
-                  id="open-btn"
-                  className="px-4 uppercase font-sans border-2 border-darkGolden rounded-md p-2 hover:bg-orange-400 hover:text-white transition ease-in-out delay-50  hover:-translate-y-1 hover:scale-110 tracking-widest"
-                >
-                  send message
-                </button>
-              )}
-            </div>
-          </form>
-        </div>
-      </motion.div>
+              <div className="flex justify-center md:justify-end md:px-7 md:mt-5 mt-6 ">
+                {messageSent ? (
+                  <button
+                    type="button"
+                    disabled
+                    className="px-4 uppercase font-sans border-2 border-darkGolden rounded-md p-2 hover:bg-orange-400 hover:text-white transition ease-in-out delay-50  hover:-translate-y-1 hover:scale-110 tracking-widest"
+                  >
+                    sending...
+                  </button>
+                ) : (
+                  <button
+                    type="button"
+                    onClick={handleSubmit(handleFormSubmit)}
+                    id="open-btn"
+                    className="px-4 uppercase font-sans border-2 border-darkGolden rounded-md p-2 hover:bg-orange-400 hover:text-white transition ease-in-out delay-50  hover:-translate-y-1 hover:scale-110 tracking-widest"
+                  >
+                    send message
+                  </button>
+                )}
+              </div>
+            </form>
+          </div>
+        </motion.div>
       </div>
     </>
   );
